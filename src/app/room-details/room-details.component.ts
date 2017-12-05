19063b8f0425de570af8547b41559e2da220c870
InsolentBacon <insolentbacon@minet.net>
2017-12-05 20:13:40 +0100

    Added ports on room page

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { RoomService } from '../api/services/room.service';
+import { PortService } from '../api/services/port.service';
 import { Room } from '../api/models/room';
+import { Port } from '../api/models/port';
 
 import { ActivatedRoute } from '@angular/router';
 
@@ -15,15 +17,17 @@ import { ActivatedRoute } from '@angular/router';
 export class RoomDetailsComponent implements OnInit, OnDestroy {
 
   room$: Observable<Room>;
+  ports$: Observable<Port[]>;
   roomNumber: number;
   private sub: any;
 
-  constructor(public roomService: RoomService, private route: ActivatedRoute) { }
+  constructor(public roomService: RoomService, public portService: PortService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe( params => {
       this.roomNumber = +params["roomNumber"];
       this.room$ = this.roomService.getRoom( this.roomNumber );
+      this.ports$ = this.portService.filterPort( { 'roomNumber': this.roomNumber } );
     });
   }
   
