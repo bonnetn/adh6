45307063986a7d8aa2e47eced277651d5a908df7
occuria <stevan.coroller@telecom-sudparis.eu>
2017-12-06 13:59:26 +0100

    Adding an ngFor to room-details' view, to manage rooms with multiple members

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -6,6 +6,8 @@ import { RoomService } from '../api/services/room.service';
 import { PortService } from '../api/services/port.service';
 import { Room } from '../api/models/room';
 import { PortSearchResult } from '../api/models/port-search-result';
+import { User } from '../api/models/user';
+import { UserService } from '../api/services/user.service';
 
 import { ActivatedRoute } from '@angular/router';
 
@@ -18,16 +20,18 @@ export class RoomDetailsComponent implements OnInit, OnDestroy {
 
   room$: Observable<Room>;
   ports$: Observable<PortSearchResult[]>;
+  members$: Observable<User[]>;
   roomNumber: number;
   private sub: any;
 
-  constructor(public roomService: RoomService, public portService: PortService, private route: ActivatedRoute) { }
+  constructor(public roomService: RoomService, public portService: PortService, public userService: UserService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe( params => {
       this.roomNumber = +params["roomNumber"];
       this.room$ = this.roomService.getRoom( this.roomNumber );
       this.ports$ = this.portService.filterPort( { 'roomNumber': this.roomNumber } );
+      this.members$ = this.userService.filterUser( { 'roomNumber': this.roomNumber } );
     });
   }
   
