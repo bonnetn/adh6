c5081777c1c3ff52f189700223e8efafc8b814b0
InsolentBacon <insolentbacon@minet.net>
2017-12-15 00:34:29 +0100

    Added delete/edit button on device details page

--- src/app/device-details/device-details.component.ts
+++ src/app/device-details/device-details.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { DeviceService } from '../api/services/device.service';
 import { Device } from '../api/models/device';
 
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -18,7 +18,16 @@ export class DeviceDetailsComponent implements OnInit, OnDestroy {
   device$: Observable<Device>;
   mac: string;
   private sub: any;
-  constructor( public deviceService: DeviceService, private route: ActivatedRoute) { }
+  constructor( 
+    public deviceService: DeviceService, 
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  onDelete( mac: string ) {
+    this.deviceService.deleteDevice( mac ).subscribe( () => {
+      this.router.navigate(["device/search"]);
+    });
+  }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe( params => {
