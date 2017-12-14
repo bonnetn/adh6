c2bb5075167774c6d2cc97018415237bba740914
InsolentBacon <insolentbacon@minet.net>
2017-12-14 23:54:51 +0100

    Added a search bar for the members

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 import {Observable} from 'rxjs/Observable';
+import { BehaviorSubject }    from 'rxjs/BehaviorSubject';
+
 
 import { UserService } from '../api/services/user.service'
 import { User } from '../api/models/user'
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
 
 @Component({
   selector: 'app-members',
@@ -13,11 +19,26 @@ import { User } from '../api/models/user'
 export class MemberListComponent implements OnInit {
 
   members$: Observable<User[]>;
+  private searchTerms = new BehaviorSubject<string>("");
   
   constructor(public userService: UserService) { }
 
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
   ngOnInit() {
-    this.members$ = this.userService.filterUser( {} );
+    this.members$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      //    debounceTime(300),
+
+      // ignore new term if same as previous term
+      //distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.userService.filterUser( {'terms':term} )),
+    );
+    //this.searchTerms.next("c");
   }
 
 }
