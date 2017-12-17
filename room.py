58b8148b70a0000834b248a1d3ec7561bdfcb067
InsolentBacon <insolentbacon@minet.net>
2017-12-17 21:31:30 +0100

    Implemented search function for Room

--- room.py
+++ room.py
@@ -1,4 +1,6 @@
 from connexion import NoContent
+from itertools import islice
+
 ROOMS = {
     1111 : {
         'description' : "Chambre 1111",
@@ -14,8 +16,21 @@ ROOMS = {
     }
 }
 
-def filterRoom( limit=100 ):
-  return list(ROOMS.values())[:limit]
+def findInRoom( room, terms ):
+    txt = ""
+    txt += room["description"] + " "
+    txt += str(room["roomNumber"]) + " "
+    txt += str(room["phone"]) + " "
+
+    return txt.lower().find( terms.lower() ) != -1
+
+def filterRoom( limit=100, terms=None ):
+  all_rooms = list(ROOMS.values())
+
+  if terms != None:
+      all_rooms = filter( lambda x: findInRoom(x, terms), all_rooms)
+
+  return list(islice(all_rooms,limit))
 
 def putRoom( roomNumber, body ):
         if roomNumber in ROOMS:
