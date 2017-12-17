af3139ae79fa0579981e918081c1273ee590a73e
InsolentBacon <insolentbacon@minet.net>
2017-12-17 21:02:35 +0100

    Improved filterDevice function

--- device.py
+++ device.py
@@ -1,4 +1,5 @@
 from connexion import NoContent
+from itertools import islice
 
 DEVICES = {
     "01:23:45:67:89:AB": {
@@ -16,6 +17,15 @@ DEVICES = {
         'username'	 : 'coroller'
     }
 }
+
+def filterDevice( limit=100, username=None ):
+
+  all_devices = list(DEVICES.values())
+
+  if username != None:
+      all_devices = filter( lambda x: x["username"]==username, all_devices ) 
+  
+  return list(islice(all_devices,limit))
 
 def putDevice( macAddress, body ):
   if macAddress in DEVICES:
@@ -43,13 +53,6 @@ def deleteDevice( macAddress ):
     return 'Not found', 404
 
 
-def filterDevice( limit=100, username=None ):
-
-  all_devices = list(DEVICES.values())
-  if username == None:
-    return all_devices[:limit]
-  
-  return list(filter( lambda x: x["username"]==username, all_devices))[:limit]
 
 
   
