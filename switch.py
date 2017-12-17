522f5c464ccc4162ceb417fb2ccd4fa767cce8be
InsolentBacon <insolentbacon@minet.net>
2017-12-17 21:43:41 +0100

    Implemented search function for Switch

--- switch.py
+++ switch.py
@@ -1,4 +1,5 @@
 from connexion import NoContent
+from itertools import islice
 
 SWITCHES = {
     1: {
@@ -16,21 +17,40 @@ SWITCHES = {
         'ip'          : "192.168.102.12",
         'community'   : "tirelinpimpon"
     },
-        
 }
+
+for k,v in SWITCHES.items():
+    v["_id"] = k
+
 switch_id = 42
 
-def filterSwitch( limit=100 ):
-  res = []
-  for k,v in SWITCHES.items():
-      res += [{ 'switchID': k, 'switch': v }]
-  return res[:limit]
+def findInSwitch( switch, terms ):
+    txt = ""
+    txt += switch["description"] + " "
+    txt += switch["ip"] + " "
+    txt += switch["community"] + " " # lol search on community :D
+    return txt.lower().find( terms.lower() ) != -1
+
+
+def filterSwitch( limit=100, terms=None ):
+  all_switches = list(SWITCHES.values())
+  
+  if terms != None:
+      all_switches = filter( lambda x: findInSwitch(x, terms), all_switches )
+  
+  all_switches = islice(all_switches, limit)
+
+  all_switches = map( lambda x: {"switchID": x["_id"], "switch": x },
+          all_switches)
+
+  return list(all_switches)
 
 def createSwitch( body ):
   global switch_id
   i = switch_id
   switch_id += 1
   SWITCHES[i] = body
+  SWITCHES[i]["_id"] = i
   return NoContent, 201, { 'Location':'/switch/{}'.format(i) }
 
 def getSwitch( switchID ):
@@ -42,6 +62,7 @@ def updateSwitch( switchID, body ):
   if switchID not in SWITCHES:
     return "Not found", 404
   SWITCHES[switchID] = body
+  SWITCHES[switchID]["_id"] = switchID
   return NoContent, 204
 
 def deleteSwitch( switchID ):
