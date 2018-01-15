3e1ebc84292aeba8f6f62612b29eae98565d87e0
InsolentBacon <insolentbacon@minet.net>
2018-01-15 11:12:17 +0100

    Added tests for switch API

--- test/test_switch.py
+++ test/test_switch.py
@@ -1,58 +1,91 @@
-from controller import switch
-import unittest
-d = { 
-  'description'     : 'Switch',
-  'ip'              : "192.168.0.1",
-  'community'       : "test"
-}
-d2 = { 
-  'description'     : 'Switch2',
-  'ip'              : "192.168.0.2",
-  'community'       : "tototo"
-}
+import pytest
+import json
+from .resource import base_url
 
 
-
-class TestSwitch(unittest.TestCase):
-
-    def test(self):
-
-      # POST
-      resultString, resultCode, headers = switch.createSwitch(d)
-      self.assertEqual(resultCode,201) # Created
-      switchID = int(headers["Location"].split("/")[-1])
-
-      # GET
-      result = switch.getSwitch(switchID)
-      self.assertEqual(result, d) # getSwitch
-
-      resultString, resultCode = switch.getSwitch(-42)
-      self.assertEqual(resultCode,404) 
-
-      # DELETE
-      resultString, resultCode = switch.deleteSwitch(switchID)
-      self.assertEqual(resultCode,204) 
-      resultString, resultCode = switch.getSwitch(switchID)
-      self.assertEqual(resultCode,404) # already deleteSwitchd!
-
-      resultString, resultCode = switch.deleteSwitch(-42)
-      self.assertEqual(resultCode,404) 
-
-      resultString, resultCode, headers = switch.createSwitch(d)
-      switchID = int(headers["Location"].split("/")[-1])
-      switchIDd2 = switchID
-
-      #UPDATE
-      switch.updateSwitch(switchID, d2)
-      result = switch.getSwitch(switchID)
-      self.assertEqual(result, d2) # 
-
-      resultString, resultCode, headers = switch.createSwitch(d)
-      switchIDd = int(headers["Location"].split("/")[-1])
-
-      result = switch.filterSwitch()
-      self.assertIn( {'switchID':switchIDd, 'switch':d}, result )
-      self.assertIn( {'switchID':switchIDd2, 'switch':d2}, result )
-     
+@pytest.fixture
+def api_client():
+    from .context import app
+    with app.app.test_client() as c:
+        yield c
 
 
+def test_valid_switch_insert(api_client):
+
+    sample_switch = {
+      "description": "Test Switch",
+      "ip": "192.168.103.128",
+      "community": "myGreatCommunity"
+    }
+
+    # Insert data to the database
+    r = api_client.post("{}/switch/".format(base_url),
+                        data=json.dumps(sample_switch),
+                        content_type='application/json')
+    assert r.status_code == 201
+    assert 'Location' in r.headers
+
+    # Make sure the data is now fetchable
+    r = api_client.get(r.headers["Location"])
+    assert r.status_code == 200, "Couldn't fetch the newly created switch"
+    assert json.loads(r.data)
+
+
+def test_switch_get_all(api_client):
+    r = api_client.get("{}/switch/".format(base_url))
+    assert r.status_code == 200
+    assert json.loads(r.data)
+
+
+def test_switch_get_existant_switch(api_client):
+    r = api_client.get("{}/switch/{}".format(base_url, 1))
+    assert r.status_code == 200
+    assert json.loads(r.data)
+
+
+def test_switch_get_non_existant_switch(api_client):
+    r = api_client.get("{}/switch/{}".format(base_url, 100000))
+    assert r.status_code == 404
+
+
+def test_switch_update_existant_switch(api_client):
+    sample_switch = {
+      "description": "Modified switch",
+      "ip": "192.168.103.132",
+      "community": "communityModified"
+    }
+
+    r = api_client.put("{}/switch/{}".format(base_url, 1),
+                       data=json.dumps(sample_switch),
+                       content_type='application/json')
+    assert r.status_code == 204
+
+    r = api_client.get("{}/switch/{}".format(base_url, 1))
+    assert r.status_code == 200
+    assert json.loads(r.data) == sample_switch, "The switch wasn't modified"
+
+
+def test_switch_update_non_existant_switch(api_client):
+    sample_switch = {
+      "description": "Modified switch",
+      "ip": "192.168.103.132",
+      "community": "communityModified"
+    }
+
+    r = api_client.put("{}/switch/{}".format(base_url, 100000),
+                       data=json.dumps(sample_switch),
+                       content_type='application/json')
+    assert r.status_code == 404
+
+
+def test_switch_delete_existant_switch(api_client):
+    r = api_client.delete("{}/switch/{}".format(base_url, 1))
+    assert r.status_code == 204
+
+    r = api_client.get("{}/switch/{}".format(base_url, 1))
+    assert r.status_code == 404
+
+
+def test_switch_delete_non_existant_switch(api_client):
+    r = api_client.delete("{}/switch/{}".format(base_url, 10000))
+    assert r.status_code == 404
