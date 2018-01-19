db82bbc69d4d29c1b920ef5ed3755d2a40860d07
InsolentBacon <insolentbacon@minet.net>
2018-01-19 15:24:27 +0100

    test_device now uses the mock objects inserted in the db

--- test/test_device.py
+++ test/test_device.py
@@ -4,24 +4,48 @@ import urllib.parse
 import pytest
 from model.database import Database as db
 from unit_test_settings import DATABASE as db_settings
-from model.models import Ordinateur, Portable
+from model.models import Ordinateur, Portable, Adherent
 
-from .resource import base_url, device_bonnet, device_cazal
+from .resource import base_url
+
+SAMPLE_MEMBER = Adherent(
+    nom='Dubois',
+    prenom='Jean-Louis',
+    mail='j.dubois@free.fr',
+    login='dubois_j',
+    password='',
+)
+SAMPLE_WIRED_DEVICE = Ordinateur(
+    mac='12:34:56:78:9A:BC',
+    ip='157.159.42.42',
+    dns='bonnet_n4651',
+    adherent_id=1,  # TODO transform this attribute into a foreign keyword
+    ipv6='e91f:e45a:0db3:15df:a4a7:6316:d8d7:1d4a'
+)
+SAMPLE_WIRELESS_DEVICE = Portable(
+    mac='12:34:56:78:9A:FF',
+    adherent_id=1,  # TODO transform this attribute into a foreign keyword
+)
+
+'''
+Device that will be inserted/updated when tests are run.
+It is not present in the api_client by default
+'''
+TEST_DEVICE = {
+  'mac': '01:23:45:67:89:AB',
+  'ipAddress': '127.0.0.1',
+  'ipv6Address': 'string',
+  'connectionType': 'wired',
+  'username': 'doe_john'
+}
 
 
 def prep_db(session):
-    sample_wired_device = Ordinateur(
-        mac="12:34:56:78:9A:BC",
-        ip="157.159.42.42",
-        dns='bonnet_n4651',
-        adherent_id=12,  # TODO transform this attribute into a foreign keyword
-        ipv6='e91f:e45a:0db3:15df:a4a7:6316:d8d7:1d4a'
-    )
-    sample_wireless_device = Portable(
-        mac="12:34:56:78:9A:FF",
-        adherent_id=12,  # TODO transform this attribute into a foreign keyword
-    )
-    session.add_all([sample_wired_device, sample_wireless_device])
+    session.add_all([
+        SAMPLE_MEMBER,
+        SAMPLE_WIRED_DEVICE,
+        SAMPLE_WIRELESS_DEVICE
+    ])
     session.commit()
 
 
@@ -35,59 +59,49 @@ def api_client():
 
 
 def test_device_list(api_client):
-    r = api_client.get(base_url + "/device/")
+    r = api_client.get(base_url + '/device/')
     assert r.status_code == 200
     response = json.loads(r.data)
     assert len(response) == 2
 
 
 def test_device_insert(api_client):
-    parsed_mac = urllib.parse.quote_plus(device_bonnet["mac"])
-    r = api_client.put(base_url + "/device/" + parsed_mac,
-                       data=json.dumps(device_bonnet),
+
+    r = api_client.put('{}/device/{}'.format(base_url, TEST_DEVICE['mac']),
+                       data=json.dumps(TEST_DEVICE),
                        content_type='application/json')
     assert r.status_code == 201
 
 
 def test_device_update(api_client):
-    parsed_mac = urllib.parse.quote_plus(device_bonnet["mac"])
-    r = api_client.put(base_url + "/device/" + parsed_mac,
-                       data=json.dumps(device_bonnet),
-                       content_type='application/json')
+    r = api_client.put(
+        '{}/device/{}'.format(base_url, SAMPLE_WIRED_DEVICE['mac']),
+        data=json.dumps(TEST_DEVICE),
+        content_type='application/json')
     assert r.status_code == 204
 
 
 def test_device_get(api_client):
-    mac = urllib.parse.quote_plus("non_existent_MAC")
-    r = api_client.get(''.join([base_url, "/device/", mac]))
+    mac = urllib.parse.quote_plus('non_existent_MAC')
+    r = api_client.get(''.join([base_url, '/device/', mac]))
     assert r.status_code == 404
 
 
 def test_device_delete(api_client):
-    parsed_mac = urllib.parse.quote_plus(device_bonnet["mac"])
-    r = api_client.delete(base_url + "/device/" + parsed_mac)
+    parsed_mac = urllib.parse.quote_plus(SAMPLE_WIRED_DEVICE['mac'])
+    r = api_client.delete(base_url + '/device/' + parsed_mac)
     assert r.status_code == 204
 
 
 def test_device_unexistant_delete(api_client):
-    mac = urllib.parse.quote_plus("non_existent_MAC")
-    r = api_client.delete(base_url + "/device/" + mac)
+    mac = urllib.parse.quote_plus('non_existent_MAC')
+    r = api_client.delete(base_url + '/device/' + mac)
     assert r.status_code == 404
 
 
 def test_device_filter(api_client):
-    parsed_mac_bonnet = urllib.parse.quote_plus(device_bonnet["mac"])
-    r = api_client.put(base_url + "/device/" + parsed_mac_bonnet,
-                       data=json.dumps(device_bonnet),
-                       content_type='application/json')
-    assert r.status_code == 201
-
-    parsed_mac_cazal = urllib.parse.quote_plus(device_cazal["mac"])
-    r = api_client.put(base_url + "/device/" + parsed_mac_cazal,
-                       data=json.dumps(device_cazal),
-                       content_type='application/json')
-    assert r.status_code == 201
-    r = api_client.get(base_url+"/device/?username="+device_cazal["username"])
+    r = api_client.get(
+        base_url+'/device/?username='+SAMPLE_WIRED_DEVICE.adherent.login)
     assert r.status_code == 200
     response = json.loads(r.data)
     assert len(response) == 1
