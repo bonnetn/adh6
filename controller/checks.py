26a1ab299bda19839c4ed5b179152942abe0de74
InsolentBacon <insolentbacon@minet.net>
2018-04-04 18:08:35 +0200

    Commented out some unused functions

--- controller/checks.py
+++ controller/checks.py
@@ -1,5 +1,5 @@
-from datetime import datetime
-from ipaddress import IPv4Address, IPv6Address, AddressValueError
+# from datetime import datetime
+from ipaddress import IPv4Address, AddressValueError
 import re
 
 MAC_REGEX = re.compile('^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$')
@@ -11,22 +11,22 @@ def isMac(macAddress):
     return bool(MAC_REGEX.match(macAddress))
 
 
-def checkDate(dateString):
-    """ Allowed date format: YYYY-MM-DD """
-    splittedDate = dateString.split('-')
-    if len(splittedDate) != 3 \
-       or len(splittedDate[0]) != 4 \
-       or len(splittedDate[1]) != 2 \
-       or len(splittedDate[2]) != 2:
-        return False
-    else:
-        try:
-            datetime(int(splittedDate[0]),
-                     int(splittedDate[1]),
-                     int(splittedDate[2]))
-        except (TypeError, ValueError):
-            return False
-    return True
+# def checkDate(dateString):
+#     """ Allowed date format: YYYY-MM-DD """
+#     splittedDate = dateString.split('-')
+#     if len(splittedDate) != 3 \
+#        or len(splittedDate[0]) != 4 \
+#        or len(splittedDate[1]) != 2 \
+#        or len(splittedDate[2]) != 2:
+#         return False
+#     else:
+#         try:
+#             datetime(int(splittedDate[0]),
+#                      int(splittedDate[1]),
+#                      int(splittedDate[2]))
+#         except (TypeError, ValueError):
+#             return False
+#     return True
 
 
 def isIPv4(ipAddress):
@@ -38,10 +38,10 @@ def isIPv4(ipAddress):
     return True
 
 
-def isIPv6(ipAddress):
-    """ Allowed format: fe80:0000:0000:0000:62eb:69ff:feec:c643 """
-    try:
-        IPv6Address(ipAddress)
-    except AddressValueError:
-        return False
-    return True
+# def isIPv6(ipAddress):
+#     """ Allowed format: fe80:0000:0000:0000:62eb:69ff:feec:c643 """
+#     try:
+#         IPv6Address(ipAddress)
+#     except AddressValueError:
+#         return False
+#     return True
