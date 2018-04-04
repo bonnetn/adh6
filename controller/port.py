f031b786eac5292a6a5d1b18afeaf1abf4128382
InsolentBacon <insolentbacon@minet.net>
2018-04-04 22:28:07 +0200

    Implemented filterPort

--- controller/port.py
+++ controller/port.py
@@ -1,64 +1,52 @@
 from connexion import NoContent
-from itertools import islice
-from store import get_db
+from model.database import Database as db
+from sqlalchemy import or_
+from model.models import Port
 
 
-def findInPort(port, terms):
-    txt = ""
-    txt += port["portNumber"] + " "
-    txt += str(port["roomNumber"]) + " "
-    return txt.lower().find(terms.lower()) != -1
+def toDict(port):
+    return {
+        "port": {
+            "roomNumber": port.chambre_id,
+            "switchID": port.switch.id,
+            "portNumber": port.numero,
+        },
+        "switchID": port.switch.id,
+        "portID": port.id
+    }
 
 
 def filterPort(limit=100, switchID=None, roomNumber=None, terms=None):
-    PORTS = get_db()["PORTS"]
-    all_ports = list(PORTS.values())
 
-    if switchID != None:
-        all_ports = filter(lambda x: x["switchID"] == switchID, all_ports)
+    q = db.get_db().get_session().query(Port)
+    if switchID:
+        q = q.filter(Port.switch_id == switchID)
+    if roomNumber:
+        q = q.filter(Port.chambre_id == roomNumber)
+    if terms:
+        q = q.filter(or_(
+            Port.numero.contains(terms),
+            Port.oid.contains(terms),
+        ))
+    q = q.limit(limit)
+    result = q.all()
 
-    if roomNumber != None:
-        all_ports = filter(lambda x: x["roomNumber"] == roomNumber, all_ports)
-
-    if terms != None:
-        all_ports = filter(lambda x: findInPort(x, terms), all_ports)
-
-    all_ports = islice(all_ports, limit)
-    all_ports = map(lambda x: {'portID': x["_id"], 'switchID': x['switchID'],
-                               'port': x}, all_ports)
-    return list(all_ports)
+    result = map(toDict, result)
+    result = list(result)
+    return result, 200
 
 
 def createPort(switchID, body):
-    PORTS = get_db()["PORTS"]
-    port_id = get_db()["port_id"]
-    i = port_id
-    port_id += 1
-    PORTS[i] = body
-    PORTS[i]["_id"] = i
-    return "Created", 201, {'Location': '/switch/{}/port/{}'.format(switchID, i)}
+    return NoContent, 500
 
 
 def getPort(switchID, portID):
-    PORTS = get_db()["PORTS"]
-    if portID not in PORTS:
-        return "Not found", 404
-    return PORTS[portID]
+    return NoContent, 500
 
 
 def updatePort(switchID, portID, body):
-    PORTS = get_db()["PORTS"]
-    if portID not in PORTS:
-        return "Not found", 404
-    PORTS[portID] = body
-    PORTS[portID]["_id"] = portID
-    return NoContent, 204
+    return NoContent, 500
 
 
 def deletePort(switchID, portID):
-    PORTS = get_db()["PORTS"]
-    if portID in PORTS:
-        del PORTS[portID]
-        return NoContent, 204
-    else:
-        return 'Not found', 404
+    return NoContent, 500
