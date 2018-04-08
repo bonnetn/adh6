9889d4a5f90b0dd6bd20aa13aa8340da899dee16
InsolentBacon <insolentbacon@minet.net>
2018-04-08 16:04:05 +0200

    Added test for room api

--- test/test_room.py
+++ test/test_room.py
@@ -60,7 +60,6 @@ def api_client(sample_room1, sample_room2, sample_vlan):
 def test_room_filter_all_rooms(api_client):
     r = api_client.get("{}/room/".format(base_url))
     assert r.status_code == 200
-
     response = json.loads(r.data.decode())
     assert len(response) == 2
 
@@ -68,6 +67,60 @@ def test_room_filter_all_rooms(api_client):
 def test_room_filter_all_rooms_limit(api_client):
     r = api_client.get("{}/room/?limit={}".format(base_url, 1))
     assert r.status_code == 200
-
     response = json.loads(r.data.decode())
     assert len(response) == 1
+
+
+def test_room_filter_by_term(api_client):
+    r = api_client.get("{}/room/?terms={}".format(base_url, "voisin"))
+    assert r.status_code == 200
+    response = json.loads(r.data.decode())
+    assert len(response) == 1
+
+
+def test_room_get_valid_room(api_client):
+    r = api_client.get("{}/room/{}".format(base_url, 4591))
+    assert r.status_code == 200
+    response = json.loads(r.data.decode())
+    assert len(response) == 4
+
+
+def test_room_get_invalid_room(api_client):
+    r = api_client.get("{}/room/{}".format(base_url, 4900))
+    assert r.status_code == 404
+
+
+def test_room_put_new_room(api_client):
+    room = {
+      "roomNumber": 5110,
+      "vlan": 45,
+      "phone": 6842,
+      "description": "Chambre 5110"
+    }
+    r = api_client.put("{}/room/{}".format(base_url, 5110),
+                       data=json.dumps(room),
+                       content_type='application/json')
+    assert r.status_code == 201
+
+
+def test_room_put_update_room(api_client):
+    room = {
+      "roomNumber": 5110,
+      "vlan": 45,
+      "phone": 6842,
+      "description": "Chambre 5110"
+    }
+    r = api_client.put("{}/room/{}".format(base_url, 4591),
+                       data=json.dumps(room),
+                       content_type='application/json')
+    assert r.status_code == 204
+
+
+def test_room_delete_existant_room(api_client):
+    r = api_client.delete("{}/room/{}".format(base_url, 4591))
+    assert r.status_code == 204
+
+
+def test_room_delete_non_existant_room(api_client):
+    r = api_client.delete("{}/room/{}".format(base_url, 4900))
+    assert r.status_code == 404
