6f11adf3dbfe2c75b5e6d371e947eb63dbcbc654
zteeed <zteeed@minet.net>
2018-04-09 23:56:36 +0200

    fix sqlite zteeed server

--- adh/model/database.py
+++ adh/model/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine.url import URL
+from sqlalchemy import create_engine, event
+
 
 Base = declarative_base()
 
@@ -11,6 +13,14 @@ class Database():
 
     def __init__(self, db_settings, testing=False):
         self.engine = create_engine(URL(**db_settings), pool_recycle=3600)
+        @event.listens_for(self.engine, "connect")
+        def do_connect(dbapi_connection, connection_record):
+            dbapi_connection.isolation_level = None
+
+        @event.listens_for(self.engine, "begin")
+        def do_begin(conn):
+            # emit our own BEGIN
+            conn.execute("BEGIN")
         self.db_session = scoped_session(sessionmaker(bind=self.engine))
         if testing:
             Base.metadata.drop_all(self.engine)
