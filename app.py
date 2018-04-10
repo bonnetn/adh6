e20c090b1151fea30df16c48f03d64aa31b91127
InsolentBacon <insolentbacon@minet.net>
2018-04-10 09:40:33 +0200

    Enabled 'strict_mode' for the API

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ from connexion.resolver import RestyResolver
 
 logging.basicConfig(level=logging.INFO)
 app = connexion.FlaskApp(__name__)
-app.add_api('swagger.yaml', resolver=RestyResolver('adh.controller'))
+app.add_api('swagger.yaml',
+            resolver=RestyResolver('adh.controller'),
+            strict_validation=True)
 CORS(app.app)
 # set the WSGI application callable to allow using uWSGI:
 # uwsgi --http :8080 -w app
