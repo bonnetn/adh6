4eb372b027467e567fc2b8d3996df5a016305d70
zteeed <zteeed@minet.net>
2018-04-12 14:11:12 +0200

    remove app component notification old

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  alert_type: string = "default";
-  alert_message_type: string = "";
-  alert_message_display: string  = "";
-  titre: string = 'ADH6';
-
-  onReset() {
-    this.alert_type = "default";
-    this.alert_message_type = "";
-    this.alert_message_display = "";
-  }
 }
 
