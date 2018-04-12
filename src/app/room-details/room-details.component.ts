b6e8d00ac9121ba903e9072211728b85eafb6c04
zteeed <zteeed@minet.net>
2018-04-12 04:48:27 +0200

    delete room added with notification

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
-
+import { HttpResponse } from '@angular/common/http';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { RoomService } from '../api/services/room.service';
 import { PortService } from '../api/services/port.service';
 import { Room } from '../api/models/room';
@@ -9,7 +10,7 @@ import { PortSearchResult } from '../api/models/port-search-result';
 import { User } from '../api/models/user';
 import { UserService } from '../api/services/user.service';
 
-import { ActivatedRoute } from '@angular/router';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-room-details',
@@ -17,6 +18,9 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./room-details.component.css']
 })
 export class RoomDetailsComponent implements OnInit, OnDestroy {
+  
+  // disabled: boolean = false;
+  private alive: boolean = true;
 
   room$: Observable<Room>;
   ports$: Observable<PortSearchResult[]>;
@@ -24,7 +28,40 @@ export class RoomDetailsComponent implements OnInit, OnDestroy {
   roomNumber: number;
   private sub: any;
 
-  constructor(public roomService: RoomService, public portService: PortService, public userService: UserService, private route: ActivatedRoute) { }
+  constructor(
+    private appcomponent: AppComponent,
+    private router: Router,
+    public roomService: RoomService, 
+    public portService: PortService, 
+    public userService: UserService, 
+    private route: ActivatedRoute
+  ) { }
+
+  onDelete() {
+    const v = this.roomNumber;
+    this.roomService.deleteRoomResponse( v )
+      .takeWhile( () => this.alive )
+      .subscribe( (response : HttpResponse<void>) => {
+        if( response.status == 204 ) {
+          this.router.navigate(["room"])
+          this.appcomponent.alert_type = "success"
+          this.appcomponent.alert_message_type = "Succès"
+          this.appcomponent.alert_message_display = "Chambre Supprimée"
+        }
+        else if (response.status == 404 ){
+          this.appcomponent.alert_type = "warning"
+          this.appcomponent.alert_message_type = "Attention"
+          this.appcomponent.alert_message_display = "Chambre Inconnue"
+        }
+        else {
+          this.appcomponent.alert_type = "danger"
+          this.appcomponent.alert_message_type = "Danger"
+          this.appcomponent.alert_message_display = "Erreur Inconnue"
+        }
+      });
+  
+    }
+
 
   ngOnInit() {
     this.sub = this.route.params.subscribe( params => {
