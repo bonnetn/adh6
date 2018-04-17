2508618d9b00a50d23ead7bbcc17404904715654
zteeed <zteeed@minet.net>
2018-04-17 14:03:42 +0200

    switch local display

--- src/app/switch-local/switch-local.component.ts
+++ src/app/switch-local/switch-local.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { SwitchService } from '../api/services/switch.service';
+import { Switch } from '../api/models/switch';
+import { Port } from '../api/models/port';
+import { PortService } from '../api/services/port.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-switch-local',
   templateUrl: './switch-local.component.html',
   styleUrls: ['./switch-local.component.css']
 })
-export class SwitchLocalComponent implements OnInit {
+export class SwitchLocalComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  switch$: Observable<Switch>;
+  ports$: Observable<Port[]>;
+  switchID: number;
+
+  constructor(public switchService: SwitchService, private route: ActivatedRoute, public portService: PortService) { }
 
   ngOnInit() {
+    this.switchID = 8;
+    this.switch$ = this.switchService.getSwitch(this.switchID);
+    this.ports$ = this.portService.filterPort( { 'switchID': this.switchID } );
+  }
+
+  ngOnDestroy() {
   }
 
 }
