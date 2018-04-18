b687e275daf16029e36da71f2c45dcb00c9270de
zteeed <zteeed@minet.net>
2018-04-18 20:00:29 +0200

    display authentication added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   titre: string = 'ADH6';
+  public token: any;
+
+  getToken() {
+    return this.token
+  }
+  
+  isAuthenticated() {
+    return this.getToken()!=null
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+  }
+
 }
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private appcomponent: AppComponent,
+    private router: Router
+  ) { }
+
+  logout(){
+    this.appcomponent.token=null
+    this.router.navigate(["dashboard"])
+  }
 
   navBarStatus: boolean = false;
 
