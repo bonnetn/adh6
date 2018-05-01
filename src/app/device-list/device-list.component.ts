3ce75eca88fe7eefdd7929f5299547f13f68039f
Jean MICHEL <jeanmichel@adh6>
2018-05-01 03:27:17 +0200

    adding pagination device list

--- src/app/device-list/device-list.component.ts
+++ src/app/device-list/device-list.component.ts
@@ -8,6 +8,14 @@ import "rxjs/add/operator/takeWhile";
 import { DeviceService } from '../api/services/device.service';
 import { Device } from '../api/models/device';
 
+import { BehaviorSubject }    from 'rxjs/BehaviorSubject';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { PagingConf } from '../paging.config'
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+
 @Component({
   selector: 'app-device-list',
   templateUrl: './device-list.component.html',
@@ -18,22 +26,38 @@ export class DeviceListComponent implements OnInit {
   devices$: Observable<Device[]>;
   alive: boolean = true;
 
+  page_number : number = 1;
+  item_count : number = 1;
+  items_per_page : number = +PagingConf.item_count;
+  private searchTerms = new BehaviorSubject<string>("");
+
   constructor(public deviceService: DeviceService, private router: Router) { }
   
-  onDelete( mac: string ) {
-    this.deviceService.deleteDevice( mac )
-      .takeWhile( () => this.alive )
-      .subscribe( () => {
-      this.refreshDevices();
-    });
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
-  refreshDevices() {
-    this.devices$ = this.deviceService.filterDevice( {} );
+  refreshDevices(page:number) : void {
+    this.devices$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.deviceService.filterDeviceResponse( { 'terms':term, 'limit':this.items_per_page, 'offset':(page-1)*this.items_per_page} )),
+      switchMap((response) => {
+        this.item_count = +response.headers.get("x-total-count")
+        this.page_number = page;  
+        return Observable.of(response.body)
+      }),
+    );
   }
+
 
   ngOnInit() {
-    this.refreshDevices();
+    this.refreshDevices(1);
   }
 
   ngOnDestroy() {
@@ -41,3 +65,6 @@ export class DeviceListComponent implements OnInit {
   }
 
 }
+
+  
+
