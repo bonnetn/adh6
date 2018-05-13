472401baf9a3aa6935293adb8d01b1e22210633e
InsolentBacon <insolentbacon@minet.net>
2018-05-13 14:45:44 +0000

    Updated init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-export FLASK_APP=app.py 
-export AUTHLIB_INSECURE_TRANSPORT=1
-export FLASK_DEBUG=1
-flask initdb
-flask run --host zteeed.fr --port 50000 
+#export FLASK_APP=app.py 
+#export AUTHLIB_INSECURE_TRANSPORT=1
+#export FLASK_DEBUG=1
+##flask initdb
+#flask run --host 192.168.102.184 --port 50000 
+uwsgi --ini oauth2-provider.ini
