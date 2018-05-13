257376a0f4a4630d7a7585120bce6f0ae823b827
InsolentBacon <insolentbacon@minet.net>
2018-05-13 14:15:10 +0000

    Changed the configuration to use adh6.minet.net

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,10 +1,9 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  loginUrl: 'http://192.168.103.175:50000/oauth/authorize',
-  redirectUri: 'http://192.168.103.175',
-  clientId: 'jgE8tWMNou0chC1lvL4ecO5c',
+  loginUrl: 'https://adh6.minet.net/oauth/authorize',
+  redirectUri: 'https://adh6.minet.net',
+  clientId: 'H4XcptJlYAWAqyxTJxybMXfi',
   scope: 'profile',
-  requireHttps: false,
   oidc: false,
 }
