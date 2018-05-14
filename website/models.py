f4e072487854ead864e0b8d7edb6a71e688e68d3
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-05-14 13:45:49 +0200

    Removed useless reference to User object

--- website/models.py
+++ website/models.py
@@ -32,18 +32,12 @@ class OAuth2Client(db.Model, OAuth2ClientMixin):
     __tablename__ = 'oauth2_client'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(
-        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    user = db.relationship('User')
 
 
 class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
     __tablename__ = 'oauth2_code'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(
-        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    user = db.relationship('User')
 
 
 class OAuth2Token(db.Model, OAuth2TokenMixin):
