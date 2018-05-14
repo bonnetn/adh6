865edcadcd25d6a659d408d858c1787a78b89253
InsolentBacon <insolentbacon@minet.net>
2018-05-14 17:18:13 +0200

    Added authentication on SNMP port functions

--- adh/controller/snmp_port.py
+++ adh/controller/snmp_port.py
@@ -1,14 +1,22 @@
-from connexion import NoContent  
+from connexion import NoContent
+from adh.auth import auth_simple_user
 
 
+@auth_simple_user
 def getPortStatus(switchID, portID):
     return 204, NoContent, True
 
+
+@auth_simple_user
 def setPortStatus(switchID, portID, state):
     return 200, NoContent
 
+
+@auth_simple_user
 def getPortVlan(switchID, portID):
     return 200, NoContent, 42
 
+
+@auth_simple_user
 def setPortVlan(switchID, portID, vlan):
     return 204, NoContent
