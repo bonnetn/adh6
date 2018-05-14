b6ac94fda78521967eb306c2ea6cf16635469d0b
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-05-14 13:24:14 +0200

    Automatically add ADH6 client on db creation

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
-import os
 from flask import Flask
-from website.models import db
+from website.models import db, OAuth2Client
 from website.oauth2 import config_oauth
 from website.routes import bp
+
+
+ADH6_IMPLICIT_ID = "H4XcptJlYAWAqyxTJxybMXfi"
 
 
 def create_app(config=None):
@@ -30,8 +32,28 @@ def setup_app(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
+        q = db.session.query(OAuth2Client)
+        q = q.filter(OAuth2Client.client_id == ADH6_IMPLICIT_ID)
+        if not q.one_or_none():
+            cl = OAuth2Client(
+                client_id=ADH6_IMPLICIT_ID,
+                client_secret="",
+                issued_at=1525600543,
+                expires_at=0,
+                redirect_uri="https://adh6.minet.net",
+                token_endpoint_auth_method="none",
+                grant_type="implicit",
+                response_type="token",
+                scope="profile",
+                client_name="adh6",
+                logo_uri="https://adh6.minet.net",
+            )
+            db.session.add(cl)
+            db.session.commit()
+
     config_oauth(app)
     app.register_blueprint(bp, url_prefix='')
+
 
 application = create_app({
     'APPLICATION_ROOT': '/oauth',
