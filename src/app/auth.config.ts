b63380f9ff6512e945d52b81551a74edd0f3190b
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-05-14 11:39:46 +0200

    Added logout url in config

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,6 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   loginUrl: 'https://adh6.minet.net/oauth/authorize',
+  logoutUrl: 'https://adh6.minet.net/oauth/logout',
   redirectUri: 'https://adh6.minet.net',
   clientId: 'H4XcptJlYAWAqyxTJxybMXfi',
   scope: 'profile',
