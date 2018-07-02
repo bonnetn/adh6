d4822e2636c74a0569799aa0778564cb81afe4e1
bonnetn <insolentbacon@minet.net>
2018-07-02 21:07:38 +0000

    Better use of observables in device-details

--- src/app/device-details/device-details.component.ts
+++ src/app/device-details/device-details.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable'
 
-import { DeviceService } from '../api/services/device.service';
-import { Device } from '../api/models/device';
+import { DeviceService } from '../api/services/device.service'
+import { Device } from '../api/models/device'
 
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router'
 
 
 @Component({
@@ -15,28 +15,28 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class DeviceDetailsComponent implements OnInit, OnDestroy {
 
-  device$: Observable<Device>;
-  mac: string;
-  private sub: any;
+  device$: Observable<Device>
+  mac: string
+
   constructor( 
     public deviceService: DeviceService, 
     private route: ActivatedRoute,
     private router: Router) { }
 
   onDelete( mac: string ) {
-    this.deviceService.deleteDevice( mac ).subscribe( () => {
-      this.router.navigate(["device/search"]);
-    });
+    this.deviceService.deleteDevice( mac ).first().subscribe( () => {
+      this.router.navigate(["device/search"])
+    })
   }
 
   ngOnInit() {
-    this.sub = this.route.params.subscribe( params => {
-      this.mac = params["mac"];
-      this.device$ = this.deviceService.getDevice( this.mac );
-    });
+    this.device$ = this.route.params.switchMap( params => {
+      this.mac = params["mac"]
+      return this.deviceService.getDevice( this.mac )
+    })
   }
+
   ngOnDestroy() {
-    this.sub.unsubscribe();
   }
 
 }
