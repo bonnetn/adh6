85d51595b76254b7144e71f930ed8ee61039fa73
bonnetn <insolentbacon@minet.net>
2018-07-03 19:03:10 +0000

    device creation is now a bit more reactive

--- src/app/device-new/device-new.component.ts
+++ src/app/device-new/device-new.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { HttpResponse } from '@angular/common/http';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import { HttpResponse } from '@angular/common/http'
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'
 
-import 'rxjs/add/operator/takeWhile';
-import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/takeWhile'
+import 'rxjs/add/operator/switchMap'
+import { empty } from 'rxjs/observable/empty'
+import { catchError, flatMap, first, finalize } from 'rxjs/operators';
 
-import { DeviceService } from '../api/services/device.service';
-import { Device } from '../api/models/device';
-import { NotificationsService } from 'angular2-notifications/dist';
+import { DeviceService } from '../api/services/device.service'
+import { Device } from '../api/models/device'
+import { NotificationsService } from 'angular2-notifications/dist'
 
 @Component({
   selector: 'app-device-new',
@@ -18,10 +20,8 @@ import { NotificationsService } from 'angular2-notifications/dist';
 
 export class DeviceNewComponent implements OnInit, OnDestroy {
   
-  disabled: boolean = false;
-  private alive: boolean = true;
-
-  deviceForm: FormGroup;
+  disabled: boolean = false
+  deviceForm: FormGroup
 
   constructor(
     public deviceService: DeviceService,
@@ -30,7 +30,7 @@ export class DeviceNewComponent implements OnInit, OnDestroy {
     private router: Router,
     private notif: NotificationsService,
   ) { 
-  this.createForm();
+  this.createForm()
   }
 
   createForm() {
@@ -38,37 +38,40 @@ export class DeviceNewComponent implements OnInit, OnDestroy {
       username: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)] ],
       mac: ['', [Validators.required, Validators.minLength(17), Validators.maxLength(17)] ],
       connectionType: ['', Validators.required ],
-    });
+    })
   }
   
   onSubmit() {
-    this.disabled = true;
-    const v = this.deviceForm.value;
+    this.disabled = true
+    const v = this.deviceForm.value
     const device: Device = {
       username: v.username,
       mac: v.mac,
       connectionType: v.connectionType
     }
-    this.deviceService.getDeviceResponse(v.mac)
-      .takeWhile( ()=> this.alive )
-      .subscribe( (response)=> { 
-        this.notif.error("Device already exists")
-      }, (reponse)=> {
-        this.deviceService.putDeviceResponse( { "macAddress": v.mac, body: device })
-          .takeWhile( ()=> this.alive )
-          .subscribe( (response)=> { 
-            this.router.navigate(["device/view", v.mac ])
-            this.notif.success(response.status + ": Success")
-          });
-      });
-    this.disabled = false;
+
+    this.deviceService.getDeviceResponse(v.mac).first().pipe(
+        flatMap( (response, index) => {
+          // if there is a response then you should not create a new device
+          this.notif.error("Device already exists in database.")
+          return empty<HttpResponse<void>>()
+        }),
+        catchError( err => {
+          return this.deviceService.putDeviceResponse( { "macAddress": v.mac, body: device }).first()
+        }),
+        finalize( () => {
+          this.disabled = false
+        }),
+    ).subscribe( response => {
+      this.router.navigate(["device/view", v.mac ])
+    })
+
   }
 
   ngOnInit() {
   }
   
   ngOnDestroy() {
-    this.alive=false;
   }
 
 }
