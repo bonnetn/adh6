0254cd15c1398506e93c4cfe83c8fe3115c72c3c
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-09-28 12:14:56 +0200

    Add config generation script

---
+++ api_server/gen_config/generate_config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+CONFIG_FILE="$DIR/../CONFIGURATION.py" 
+
+cp "$DIR/CONFIGURATION.template" "$CONFIG_FILE"
+sed -i "s#{{ CAS_PROFILE_URL }}#${CAS_PROFILE_URL}#g" $CONFIG_FILE
+sed -i "s#{{ APPLICATION_ROOT }}#${APPLICATION_ROOT}#g" $CONFIG_FILE
+
