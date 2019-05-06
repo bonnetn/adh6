94043f76b0be4fbd7757d84ba9e185ebf7d1c7c0
Frazew <frazew@minet.net>
2019-05-07 00:24:51 +0200

    Add connection to mysql within the API when deploying

--- api_server/gen_config/generate_config.sh
+++ api_server/gen_config/generate_config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-CONFIG_FILE="$DIR/../CONFIGURATION.py" 
+CONFIG_FILE="$DIR/../config/CONFIGURATION.py" 
 
 cp "$DIR/CONFIGURATION.template" "$CONFIG_FILE"
 sed -i "s#{{ CAS_PROFILE_URL }}#${CAS_PROFILE_URL}#g" $CONFIG_FILE
