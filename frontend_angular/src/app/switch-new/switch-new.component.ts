853af5f761b5d13112d15702ff5af9b875f0e36a
Frazew <frazew@minet.net>
2019-05-09 23:09:28 +0200

    Fix switch creation and listing in frontend

--- frontend_angular/src/app/switch-new/switch-new.component.ts
+++ frontend_angular/src/app/switch-new/switch-new.component.ts
@@ -1,6 +1,8 @@
+import {Router} from "@angular/router";
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NotificationsService} from 'angular2-notifications'
 import {SwitchService} from '../api/api/switch.service';
 import {ModelSwitch} from '../api/model/modelSwitch';
 
@@ -13,10 +15,14 @@ export class SwitchNewComponent implements OnInit {
 
   switches$: Observable<Array<ModelSwitch>>;
   switchForm: FormGroup;
+  disabled = false;
+  private alive = true;
 
   constructor(
     private fb: FormBuilder,
     public switchService: SwitchService,
+    private router: Router,
+    private notif: NotificationsService,
   ) {
     this.createForm();
   }
@@ -24,6 +30,24 @@ export class SwitchNewComponent implements OnInit {
   createForm() {
     this.switchForm = this.fb.group({
       ip: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(15)]],
+      description: ['', Validators.required],
+      community: ['', Validators.required],
+    });
+  }
+  
+  onSubmit() {
+    const v = this.switchForm.value;
+    const varSwitch: ModelSwitch = {
+      description: v.description,
+      ip: v.ip,
+      community: v.community,
+    };
+
+   this.switchService.switchPost(varSwitch)
+      .takeWhile(() => this.alive)
+      .subscribe((res) => {
+        this.router.navigate(['/switch/search']);
+        this.notif.success(res.status + ': Success');
     });
   }
 
