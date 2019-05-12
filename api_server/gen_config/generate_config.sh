cecfda6ddef813fe54c318fcbf59db630490e382
Nicolas Bonnet <mail@nicolasbon.net>
2019-05-12 14:38:28 +0200

    Remove gen_config folder
    Now in dockerfile

--- api_server/gen_config/generate_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-CONFIG_FILE="$DIR/../config/CONFIGURATION.py" 
-
-cp "$DIR/CONFIGURATION.template" "$CONFIG_FILE"
-sed -i "s#{{ CAS_PROFILE_URL }}#${CAS_PROFILE_URL}#g" $CONFIG_FILE
-sed -i "s#{{ APPLICATION_ROOT }}#${APPLICATION_ROOT}#g" $CONFIG_FILE
-
