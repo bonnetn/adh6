ec39a0bf998e8c391591337f130cc44a8b7ff53d
littlewillow <littlewillow@minet.net>
2019-05-20 22:56:58 +0200

    Finish AccountHandler's search function
    in interface_adapter/http_api/account.py

--- api_server/src/interface_adapter/http_api/account.py
+++ api_server/src/interface_adapter/http_api/account.py
@@ -3,11 +3,18 @@ import requests
 from connexion import NoContent
 
 from src.constants import DEFAULT_LIMIT, DEFAULT_OFFSET
+from src.exceptions import AccountNotFoundError, UserInputError
+from src.interface_adapter.http_api.util.error import bad_request
+
 from src.interface_adapter.sql.decorator.auth import auth_regular_admin
 from src.interface_adapter.sql.decorator.sql_session import require_sql
+
 from src.use_case.account_manager import AccountManager
 from src.entity.account import Account
 from src.entity.account_type import AccountType
+
+from src.util.log import LOG
+from src.util.context import log_extra
 
 
 class AccountHandler:
@@ -16,19 +23,22 @@ class AccountHandler:
 
     @require_sql
     @auth_regular_admin
-    def search(self, limit=DEFAULT_LIMIT, offset=DEFAULT_OFFSET, terms=None):
-        # TODO: LOG.debug
+    def search(self, ctx, limit=DEFAULT_LIMIT, offset=DEFAULT_OFFSET, terms=None, name=None):
+
+        LOG.debug("http_account_search_called", extra=log_extra(ctx,
+                                                               limit=limit,
+                                                               offset=offset,
+                                                               terms=terms))
         try:
             result, count = self.account_manager.get_by_name(ctx, limit, offset, name, terms)
-        except Exception:
-            pass
-        # TODO: except NameInputError
+            headers = {
+                "X-Total-Count": count,
+                'access-control-expose-headers': 'X-Total-Count'
+            }
+            return list(map(_map_account_to_http_response, result)), 200, headers
 
-        headers = {
-            "X-Total-Count": count,
-            "access-control-expose-headers": "X-Total-Count"
-        }
-        return list(map(_map_account_to_http_response, result)), 200, headers
+        except UserInputError as e:
+            return bad_request(e), 400  # 400 Bad Request
 
     @require_sql
     @auth_regular_admin
@@ -37,8 +47,14 @@ class AccountHandler:
 
     @require_sql
     @auth_regular_admin
-    def get(self, account_id):
-        pass
+    def get(self, ctx, name):
+        """ Get a specific account. """
+        LOG.debug("http_account_get_called", extra=log_extra(ctx, name=name))
+        try:
+            return _map_account_to_http_response(self.account_manager.get_by_name(ctx, name)), 200  # 200 OK
+
+        except AccountNotFoundError:
+            return NoContent, 404  # 404 Not Found
 
     @require_sql
     @auth_regular_admin
