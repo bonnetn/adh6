ab15c50b35e1e3e43d28b9b365296c3e6e5219f9
Gabriel Prevault <gabery@minet.net>
2019-05-25 18:51:02 +0200

    Get account id from route to display account information on account view

--- frontend_angular/src/app/account-view/account-view.component.ts
+++ frontend_angular/src/app/account-view/account-view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, combineLatest } from "rxjs";
+import {map, share, switchMap} from 'rxjs/operators';
+import { AccountService } from "../api/api/account.service";
+import { Account } from "../api/model/account";
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-account-view',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-view.component.css']
 })
 export class AccountViewComponent implements OnInit {
+  account$: Observable<Account>;
 
-  constructor() { }
+  private id$: Observable<number>;
+
+  constructor(
+    private accountService: AccountService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    // id of the account
+    this.id$ = this.route.params.pipe(
+      map(params => params['accountID'])
+    );
+
+    // stream, which will emit the account id every time the page needs to be refreshed
+    const refresh$ = combineLatest([this.id$])
+      .pipe(
+        map(([x]) => x),
+      );
+
+    this.account$ = refresh$.pipe(
+      switchMap(id => this.accountService.accountIdGet(id)),
+      share()
+    );
   }
 
   goBack() {
